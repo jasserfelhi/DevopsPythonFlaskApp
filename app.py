53d6833f0a1f885914f6b0182674b210c608086a
jasserfelhi <felhijasser0@gmail.com>
2025-07-05 14:31:59 +0100

    second commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
-    return "Hello from Flask in Docker!"
+    return "JasserFelhi Greeting You from Flask in Docker!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
